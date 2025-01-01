03399b393c5af37258e414e4178a6c8adc00fef8
MarijnSt <marijn.stammeleer@gmail.com>
2025-01-01 19:14:17 +0100

    feat: add username to recipes serializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Recipe
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
 
 class RecipeSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer(read_only=True)
     class Meta:
         model = Recipe
         fields = ['title', 'ingredients', 'instruction', 'cooking_time', 'created_at', 'updated_at', 'created_by']
