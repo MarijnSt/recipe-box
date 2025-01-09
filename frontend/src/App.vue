c690ad57cf1596e7c43f03826247c11f98587ca9
MarijnSt <marijn.stammeleer@gmail.com>
2025-01-09 15:30:28 +0100

    feat: mobile friendly menu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,28 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
+import SearchIcon from '@/components/icons/SearchIcon.vue'
+import UserIcon from '@/components/icons/UserIcon.vue'
+const isMenuOpen = ref(false)
+
+function toggleMenu() {
+  isMenuOpen.value = !isMenuOpen.value
+}
 </script>
 
 <template>
   <header>
       <nav>
-        <div class="nav-left">
-          <RouterLink to="/">Recipes</RouterLink>
-        </div>
-        <div class="nav-center">
-          <RouterLink to="/">
-            <h2>Recipe Box</h2>
-          </RouterLink>
-        </div>
+        <RouterLink to="/" class="nav-center">
+          <h2>Recipe Box</h2>
+        </RouterLink>
         <div class="nav-right">
-          <RouterLink to="/about">About</RouterLink>
-          <RouterLink to="/login">Login</RouterLink>
-          <RouterLink to="/search">Search</RouterLink>
+          <RouterLink to="/search">
+            <SearchIcon />
+          </RouterLink>
+          <RouterLink to="/login">
+            <UserIcon />
+          </RouterLink>
         </div>
       </nav>
   </header>
@@ -25,61 +31,31 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped lang="scss">
-header {
-  padding: 2rem 0;
-  margin-bottom: 2rem;
-  
-  nav {
+header nav {
+  max-width: var(--container-width);
+  padding: var(--container-padding);
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  .nav-right {
+    position: absolute;
+    right: var(--container-padding);
     display: flex;
     align-items: center;
-    justify-content: space-between;
-    max-width: 1280px;
-    margin: 0 auto;
-    padding: 0 2rem;
-    
-    .nav-left, .nav-right {
-      display: flex;
-      gap: 2rem;
-      flex: 1;
-      align-items: center;
-    }
-    
-    .nav-left {
-      justify-content: flex-start;
-    }
-    
-    .nav-right {
-      justify-content: flex-end;
-    }
-    
-    .nav-center {
-      flex: 2;
-      text-align: center;
-      margin: 0;
-      h2 {
-        font-size: 3rem;
-      }
-    }
+    gap: 1rem;
 
     a {
-      font-size: 1.25rem;
-      font-weight: 500;
-      color: var(--color-primary);
-      text-decoration: none;
-      padding: 0.5rem 0;
-      transition: border-bottom 0.4s;
-      white-space: nowrap;
-      border-bottom: 1px solid transparent;
-
-      &:hover {
-        border-bottom: 1px solid var(--color-primary-light);
-      }
-
-      &.router-link-active {
-        border-bottom: 1px solid var(--color-primary);
-      }
+      height: var(--icon-size);
     }
   }
 }
 
+@media (max-width: 768px) {
+  header nav {
+    justify-content: left;
+  }
+}
 </style>
