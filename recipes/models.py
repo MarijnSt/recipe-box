93ba68dc490b9e26a230ff1429160334507f4f57
MarijnSt <marijn.stammeleer@gmail.com>
2025-01-10 15:32:43 +0100

    feat: backend changes for slugs

--- recipes/models.py
+++ recipes/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils.text import slugify
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
     ingredients = models.TextField(help_text="Enter ingredients each on a new line")
     instruction = models.TextField()
     cooking_time = models.IntegerField(help_text="Enter cooking time in minutes")
@@ -13,6 +14,11 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     class Meta:
-        ordering = ['created_at']
+        ordering = ['-created_at']
