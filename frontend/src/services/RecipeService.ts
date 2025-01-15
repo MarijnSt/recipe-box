805c1cfc36c82d99d18ce6d72196d26d9e1e7bca
MarijnSt <marijn.stammeleer@gmail.com>
2025-01-15 17:41:47 +0100

    Add method to fetch categories and category type

--- frontend/src/services/RecipeService.ts
+++ frontend/src/services/RecipeService.ts
@@ -1,6 +1,15 @@
 import type { Recipe } from "@/types/Recipe";
+import type { RecipeCategory } from "@/types/RecipeCategory";
 
 export const RecipeService = {
+  async getCategories(): Promise<RecipeCategory[]> {
+    const response = await fetch("http://localhost:8000/api/recipes/categories");
+    if (!response.ok) {
+      throw new Error('Failed to fetch categories');
+    }
+    return response.json();
+  },
+
   async getRecipes(): Promise<Recipe[]> {
     const response = await fetch("http://localhost:8000/api/recipes");
     return response.json();
