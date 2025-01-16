d0fb387ae4598c46cb67b0b7c91be607f20f4b98
MarijnSt <marijn.stammeleer@gmail.com>
2025-01-16 15:09:20 +0100

    Update recipes view to filter based on category and created by

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,21 @@
 from rest_framework import viewsets, permissions
+from rest_framework.pagination import PageNumberPagination
 from .models import Recipe
 from .serializers import RecipeSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .constants import RecipeCategory
 
+class RecipePagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     lookup_field = 'slug'
-
+    pagination_class = RecipePagination
     def get_permissions(self):
         """unauthenticated users can list and retrieve recipes, but cannot create, update, or delete"""
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
@@ -24,7 +30,19 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """all recipes for list and retrieve actions, only recipes created by the current user for create, update, and delete actions"""
         if self.action in ['list', 'retrieve']:
-            return Recipe.objects.all()
+            queryset = Recipe.objects.all()
+
+            # Filter by category
+            category = self.request.query_params.get('category', None)
+            if category:
+                queryset = queryset.filter(category=category)
+
+            # Filter by user
+            user = self.request.query_params.get('created_by', None)
+            if user:
+                queryset = queryset.filter(created_by=user)
+
+            return queryset
 
         return Recipe.objects.filter(created_by=self.request.user)
 
